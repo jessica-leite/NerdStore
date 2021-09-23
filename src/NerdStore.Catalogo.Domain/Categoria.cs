0a00136892d994b99111e9b1d8846e854371275a
Jessica Leite <jessica.mleite@gmail.com>
2021-09-23 07:47:10 -0300

    mapeamentos

--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -1,4 +1,6 @@
 ﻿using NerdStore.Core.DomainObjects;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace NerdStore.Catalogo.Domain
 {
@@ -6,7 +8,12 @@ namespace NerdStore.Catalogo.Domain
     {
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
+        
+        // EF Relation
+        public ICollection<Produto> Produtos { get; set; }
 
+        //EF tem problema para popular objetos que não têm o construtor sem parâmetros
+        protected Categoria() { }
         public Categoria(string nome, int codigo)
         {
             Nome = nome;
